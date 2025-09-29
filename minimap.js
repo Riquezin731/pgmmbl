a1f46f7e39132bfbd826ccdec081f6a248585b2f
Riquezin731 <henrique.moya.alves@escola.pr.gov.br>
2025-09-29 14:59:43 -0300

    Update minimap.js
    CC

--- minimap.js
+++ minimap.js
@@ -1,10 +1,8 @@
-import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
 
 export class Minimap {
-  constructor(mainScene, bots) {
-    // Cena separada apenas para minimapa
-    this.scene = mainScene;
-    this.bots = bots;
+  constructor(scene) {
+    this.scene = scene;
 
     const width  = 200;
     const height = 200;
@@ -14,15 +12,15 @@ export class Minimap {
     });
     this.renderer.setSize(width, height);
 
-    // Câmera ortográfica de topo
     const aspect = width / height;
-    const d = 20;
+    const d = 25;
     this.camera = new THREE.OrthographicCamera(
       -d * aspect, d * aspect,
        d, -d,
       0.1, 100
     );
     this.camera.position.set(0, 30, 0);
+    this.camera.up.set(0, 0, -1);
     this.camera.lookAt(0, 0, 0);
   }
 
